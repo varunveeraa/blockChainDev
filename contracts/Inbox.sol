6b09c592d4d42c09d9b5fdfa1f76fada2f872eae
Varun <vvee0008@student.monash.edu>
2024-08-12 11:04:25 +1000

    second commit

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.17;
 contract Inbox {
     string public message;
 
-    // Use the constructor keyword instead of a function with the same name as the contract
     constructor(string initialMessage) public {
         message = initialMessage;
     }
